ad52d34bb67592721567a36704e0edb412339e62
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2024-08-31 22:22:43 +0600

    first components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import "./App.css";
+import Gallery from "./components/Gallery";
 
 function App() {
 
   return (
     <div>
       <h1>describingTheUI</h1>
+      <Gallery></Gallery>
     </div>
   );
 }
---
+++ src/components/Gallery.jsx
@@ -0,0 +1,20 @@
+
+export default function Gallery() {
+  return (
+    <div>
+        <h3>Frist components</h3>
+        <Profile></Profile>
+    </div>
+  )
+}
+
+
+export  function Profile() {
+  return (
+    <div>
+        <h2>Profile components</h2>
+    </div>
+  )
+}
+
+
